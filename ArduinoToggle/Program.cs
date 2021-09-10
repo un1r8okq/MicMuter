ee21a5cf9b1be8056069f757b05bcd05f2513600
Will Verland <will@ver.land>
2021-09-10 17:21:33 +1200

    Add TODO to increase serial resilience

--- ArduinoToggle/Program.cs
+++ ArduinoToggle/Program.cs
@@ -8,6 +8,7 @@ namespace ArduinoToggle
     class Program
     {
         // TODO: Log time alongside error messages
+        // TODO: Improve resiliance when serial connection lost
         // TODO: Improve latency
         // TODO: Mutes all microphones, not just the current one.
         // TODO: Collect stats on use? Time spent muted/unmuted, total mute/unmutes? Graph over time?
